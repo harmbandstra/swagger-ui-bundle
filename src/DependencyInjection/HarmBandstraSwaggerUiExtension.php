1667c1e92f464bbda290d961ea523a1d94cfb47b
Harm Bandstra <hbandstra@gmail.com>
2017-02-24 10:19:44 +0100

    rename extension class

--- src/DependencyInjection/HarmBandstraSwaggerUiExtension.php
+++ src/DependencyInjection/HarmBandstraSwaggerUiExtension.php
@@ -5,7 +5,7 @@ namespace HarmBandstra\SwaggerUiBundle\DependencyInjection;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\HttpKernel\DependencyInjection\Extension;
 
-class HarmBandstraSwaggerUiBundleExtension extends Extension
+class HarmBandstraSwaggerUiExtension extends Extension
 {
     public function load(array $configs, ContainerBuilder $container)
     {
