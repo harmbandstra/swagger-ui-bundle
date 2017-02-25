44845eefee1dc562f4ef134cdb3ec5ad97ec31be
Harm Bandstra <hbandstra@gmail.com>
2017-02-25 15:14:56 +0100

    removed unused use statement

--- src/Controller/DocsController.php
+++ src/Controller/DocsController.php
@@ -8,7 +8,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
-use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 use Symfony\Component\Yaml\Yaml;
 
 class DocsController extends Controller
