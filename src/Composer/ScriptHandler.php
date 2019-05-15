70955b635a720f9226a2d327a71e28bd712f2a9a
Gareth Arnott <gareth.arnott@thirdbridge.com>
2019-05-15 17:11:42 +0100

    14 exclude map files from copy of assets

--- src/Composer/ScriptHandler.php
+++ src/Composer/ScriptHandler.php
@@ -4,6 +4,7 @@ namespace HarmBandstra\SwaggerUiBundle\Composer;
 
 use Composer\Script\Event;
 use Symfony\Component\Filesystem\Filesystem;
+use Symfony\Component\Finder\Finder;
 
 class ScriptHandler
 {
@@ -18,7 +19,10 @@ class ScriptHandler
         $source = sprintf('%s/swagger-api/swagger-ui/dist', $vendorDir);
         $target = sprintf('%s/harmbandstra/swagger-ui-bundle/src/Resources/public', $vendorDir);
 
-        $filesystem->mirror($source, $target, null, ['override' => true]);
+        $exclude = new Finder();
+        $exclude->files()->in($source)->notName('*.map');
+
+        $filesystem->mirror($source, $target, $exclude, ['override' => true]);
 
         $event->getIO()->write('Linked SwaggerUI assets.');
     }
